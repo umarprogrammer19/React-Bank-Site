b6697723367a021788171d1252e428c8c9a26b29
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-14 23:54:24 +0500

    Set Up Route For Features

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './layout.jsx';
+import Features from './pages/features.jsx';
 
 const router = createBrowserRouter([{
   path: "/",
@@ -11,7 +12,10 @@ const router = createBrowserRouter([{
   children: [{
     path: "",
     element: <App />,
-  },]
+  }, {
+    path: "features",
+    element: <Features />
+  }]
 }])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
