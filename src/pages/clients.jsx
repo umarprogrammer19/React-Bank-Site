f72b7bb5958aea3d166114b7d906c4ab09c19cea
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-15 00:27:25 +0500

    Added Clients

--- src/pages/clients.jsx
+++ src/pages/clients.jsx
@@ -1,5 +1,9 @@
+import Testimonials from "../components/Testimonials";
+
 export default function Clients() {
     return (
-        <h1>Clients</h1>
+        <div className="container px-5 md:px-10 mx-auto">
+            <Testimonials />
+        </div>
     )
 }
